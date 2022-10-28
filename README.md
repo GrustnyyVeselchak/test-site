694fd76b27bf88ad29f0524c972462b2d96f939a
Emil <113785577+GrustnyyVeselchak@users.noreply.github.com>
2022-10-28 19:40:24 +0400

    First

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # test-site
-Испытательный полигон фласка
+Испытательный полигон/обучение работы в Flask:
+https://www.youtube.com/watch?v=Y_oyx36AdV0&list=PLlWXhlUMyooZr5R2u2Zwxt6Pw6iwBo5y5
---
+++ app.py
@@ -0,0 +1,20 @@
+from cgitb import html
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template('index.html', title="ITHub")
+
+@app.route("/education")
+def education():
+    return render_template('education.html', title="Обучение")
+
+@app.route("/callboard")
+def callboard():
+    return render_template('callboard.html', title="Доска объявлений")
+
+if __name__=="__main__":
+    app.run(debug=True)
