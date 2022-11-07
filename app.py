a54e839a97729e7f138d34dc7e9ca68230899aa3
Emil <113785577+GrustnyyVeselchak@users.noreply.github.com>
2022-11-07 10:00:14 +0400

    Loginka mat' eyo

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, flash, session, redirect
 
 app = Flask(__name__)
+app.secret_key = 'jdslajfsl;afjslkfdjsal;fja'
+app.config['SECTRET_KEY'] = 'sdajflksahgsdjfaf;isoajalk'
 
 @app.route("/")
 @app.route("/index")
@@ -14,6 +16,17 @@ def education():
 @app.route('/callboard')
 def callboard():
     return render_template('callboard.html', title="Доска объявлений")
+
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['email'] != "admin" or request.form['pass'] !="admin":
+            error = "try again"
+        else:
+            return redirect(url_for('index'))
+    return render_template('login.html', title='Авторизация', error=error)
+
 
 if __name__=="__main__":
     app.run(debug=True)
